f7e443b08d7d2e1d1dd568c78d3c24f708a1ae85
barreto-jpedro <joaopedro.bcalmeida@gmail.com>
2022-03-13 13:05:40 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from open_json import build_bills_list,read_database
+from openpyxl import Workbook
+
+
+def populate_sheet(sheet, data):
+    title = list()
+    aux = list()
+    for produto in data:
+        new_item = list()
+        for k, v in produto.items():
+            new_item.append(v)
+            if k in title:
+                continue
+            else:
+                title.append(k)
+        aux.append(new_item.copy())
+    aux.insert(0,title)
+
+    for item in aux:
+        c = 1
+        for info in item:
+            sheet.cell(row=(aux.index(item)+1), column=c, value=info)
+            c += 1
+
+
+def new_excel_file():
+    path = r'C:\Users\João Pedro Barreto\Desktop\Python\Exercícios do Breno\Médio\Ex.02\database.json'
+    database = read_database(path)
+    bills = build_bills_list(database)
+    arquivo_excel = Workbook()
+    planilha1 = arquivo_excel.active
+    planilha1.title = "Produtos"
+    planilha2 = arquivo_excel.create_sheet("Nota")
+    populate_sheet(planilha1, database['products'])
+    populate_sheet(planilha2, bills)
+
+    arquivo_excel.save(filename="database.xlsx")
+
+
+def main():
+    new_excel_file()
+
+
+if __name__ == "__main__":
+    main()
---
+++ open_json.py
@@ -0,0 +1,70 @@
+import json
+
+
+def read_database(path: str) -> dict:
+    try:
+        with open(path, "r") as json_file:
+            json_object = json.load(json_file)
+            json_file.close()
+            return json_object
+    except FileNotFoundError:
+        print('ERRO: arquivo não encontrado')
+        return {}
+    except Exception as err:
+        print('Ocorreu um erro desconhecido. Segue o erro:\n', err)
+        return {}
+
+
+def build_bills_list(database: dict):
+    bills_builded = list()
+    new_item = dict()
+    for bill in database.get('bill', []):
+        items = bill.get('items', [])
+        for item in items:
+            new_item['id'] = item.get('id', '')
+            product = get_product_by_id(database, new_item['id'])
+            new_item['name'] = product.get('name', None)
+            new_item['quantity'] = item.get('quantity', None)
+            if product.get('weight', False):
+                new_item['weight'] = 'False'
+                new_item['total_price'] = item.get(
+                    'quantity', 0) / product.get('measure_quantity', 0) * product.get('unit_price', 0)
+                new_item['quantity'] = str(
+                    new_item['quantity']) + ' ' + product.get('measure', 0)
+            else:                
+                new_item['total_price'] = item.get(
+                    'quantity', 0) * product.get('unit_price', 0)
+                new_item['quantity'] = str(
+                    new_item['quantity']) + ' unidade(s)'
+
+            new_item['weight'] = product.get('weight', 'False')
+            new_item['unit_price'] = product.get('unit_price', 0)
+            new_item['measure'] = product.get('measure', 'none')
+            new_item['total_price'] = round(new_item['total_price'], 2)
+            bills_builded.append(new_item.copy())
+    return bills_builded
+
+
+def get_product_by_id(database: dict, id: int):
+    for product in database.get('products', []):
+        if product['id'] == id:
+            return product
+    else:
+        return {}
+
+
+def show_bill(path: str):
+    database = read_database(path)
+    bills = build_bills_list(database)
+    if bills:
+        total = 0
+        for bill in bills:
+            print('{:<70}'.format(bill.get('name', '')), end='')
+            print('{:^20}'.format(bill.get('quantity', 0)), end='')
+            print('{:>20}'.format(bill.get('total_price', 0)))
+            total += bill.get('total_price', 0)
+        print('-'*110)
+        print('{:<80}{:>30}'.format('TOTAL', round(total, 2)))
+        print('\n\nTotal de produtos verificados: ', len(bills))
+    else:
+        print('Não existem notas!')
